92b1745d3230dd41e664b701e9366fac3906fc5b
Piotr Pomienski <p.pomien@gmail.com>
2018-06-06 18:37:04 +0100

    basic net changes

--- brian/basic_net.py
+++ brian/basic_net.py
@@ -2,14 +2,15 @@
 from brian2 import *
 import matplotlib.pyplot as plt
 import time
-#BrianLogger.log_level_diagnostic()
-#set_device('cpp_standalone')
+BrianLogger.log_level_diagnostic()
+set_device('cpp_standalone')
 
 eqs = '''
 dv/dt = (I-v)/tau : 1
 I : 1
 tau : second
 '''
+N = 2
 G = NeuronGroup(2, eqs, threshold='v>1', reset='v = 0', method='exact')
 G.I = [2, 0]
 G.tau = [10, 100]*ms
@@ -20,10 +21,9 @@ S.connect(i=0, j=1)
 
 M = StateMonitor(G, 'v', record=True)
 
-s=time.time()
+#s=time.time()
 run(200*ms)
-e=time.time()
-print(e-s)
+#e=time.time()
 '''
 plt.plot(M.t/ms, M.v[0], label='Neuron 0')
 plt.plot(M.t/ms, M.v[1], label='Neuron 1')
